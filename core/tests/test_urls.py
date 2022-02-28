df86377559084bd3c72c2ee553ed1ef33cb385fb
VishDroid-dev <vk2345732@gmail.com>
2022-02-28 18:24:09 +0530

    added url testing

--- core/tests/test_urls.py
+++ core/tests/test_urls.py
@@ -5,25 +5,36 @@ from core.views import index, user_login,logout_view, register, detailedView,sea
 
 class TestUrls(SimpleTestCase):
     
-    def test_list_url_is_resolved(self):
+    def test_index_url_is_resolved(self):
         url = reverse('core:index')
         self.assertEquals(resolve(url).func, index)
 
+    def test_login_url_is_resolve(self):
         url = reverse('core:login')
         self.assertEquals(resolve(url).func, user_login)
 
+    def test_logout_url_is_resolves(self):
         url = reverse('core:logout')
         self.assertEquals(resolve(url).func, logout_view)
 
-
+    def test_search_url_is_resolves(self):
         url = reverse('core:search')
         self.assertEquals(resolve(url).func, search)
-
+    
+    def test_blog_url_is_resolves(self):
         url = reverse('core:blog')
         self.assertEquals(resolve(url).func, blog)
 
+    def test_add_notes_url_is_resolves(self):
         url = reverse('core:add-notes')
         self.assertEquals(resolve(url).func, add_notes)
 
+    def test_signupp_url_is_resolves(self):
         url = reverse('core:signup')
         self.assertEquals(resolve(url).func, register)
+
+
+    def test_detailed_blog_url_is_resolves(self):
+        url = reverse('core:detailed-blog', args=['some-slug'])
+        self.assertEquals(resolve(url).func, detailedView)
+    
